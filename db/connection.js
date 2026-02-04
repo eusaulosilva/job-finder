92ecf0f3ed4556a67bf49f2c86d60322f3a220a1
Saulo Silva <149823114+eusaulosilva@users.noreply.github.com>
2026-02-04 03:10:48 -0300

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const pg = require('pg'); // <--- Importamos explicitamente
 
 // A Vercel/Neon preenche a POSTGRES_URL automaticamente
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectModule: pg, // <--- Forçamos o Sequelize a usar este módulo
     dialectOptions: {
@@ -14,3 +14,4 @@ const sequelize = new Sequelize(process.env.POSTGRES_URL, {
 });
 
 module.exports = sequelize;
+
