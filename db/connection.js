66375fc42f9d535d38ba563b83859dd59838d749
Saulo Silva <149823114+eusaulosilva@users.noreply.github.com>
2026-02-04 03:14:37 -0300

    Modify SSL settings in database connection
    Updated SSL options to reject unauthorized certificates.

--- db/connection.js
+++ db/connection.js
@@ -8,10 +8,11 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false
+            rejectUnauthorized: true
         }
     }
 });
 
 module.exports = sequelize;
+
 
